2665a1ea6d1842424b1ea27de258d0e24e1867a4
Ben Belcourt <ben.belcourt@gmail.com>
2013-04-04 22:54:02 -0400

    Initial dployapp source import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dployapp::Application.config.secret_token = '945c74848b830f9659b9949e3856bef18d0fd2936a8b25aa0aba65dd94dbc5db18405c581354c08a2fbf66d8cf821159e75b5de28ce6682b4110c9062395eb70'
